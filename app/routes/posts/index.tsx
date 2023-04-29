087cec80f10d87066a90649d55f9ad425e473f41
Mario Lisbona <101855037+MarioLisbona@users.noreply.github.com>
2023-04-29 12:13:36 +1000

    Complete Loading Data section

--- app/routes/posts/index.tsx
+++ app/routes/posts/index.tsx
@@ -1,7 +1,34 @@
+import { json } from "@remix-run/node";
+import { Link, useLoaderData } from "@remix-run/react";
+
+export const loader = async () => {
+  return json({
+    posts: [
+      {
+        slug: "my-first-post",
+        title: "My First Post",
+      },
+      {
+        slug: "90s-mixtape",
+        title: "A mix tape I made for you",
+      },
+    ],
+  });
+};
+
 export default function Posts() {
+  const { posts } = useLoaderData<typeof loader>();
+  console.log(posts);
   return (
     <main>
       <h1>Posts</h1>
+      <ul>
+        {posts.map((post, index) => (
+          <li key={index}>
+            <Link to={post.slug} className="text-blue-600 underline">{post.title}</Link>
+          </li>
+        ))}
+      </ul>
     </main>
   );
 }
